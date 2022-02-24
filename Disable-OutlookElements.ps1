cc0ffb4c1cd3c6a8082e08aa7503bfac4a671843
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2022-02-24 12:14:09 +0100

    Create Disable-OutlookElements.ps1

---
+++ Disable-OutlookElements.ps1
@@ -0,0 +1,22 @@
+function Disable-OutlookElements {
+    param (
+    [Parameter(Mandatory=$true)][string]$hostname
+    )
+    $tnc = tnc $hostname
+    if ($tnc.PingSucceeded) {
+        if ((Invoke-command -computername $hostname {Get-ItemProperty 'HKLM:\Software\Microsoft\Office\Outlook\Addins\Gecko.Ephorte.Outlook.Main' -Name LoadBehavior}) -eq 0) {
+            "Outlook Elements is already deactivated"
+        } else {
+
+            $response = Invoke-command -computername $hostname {New-Item 'HKLM:\Software\Microsoft\Office\Outlook\Addins\Gecko.Ephorte.Outlook.Main' -Force | Out-Null; (New-ItemProperty 'HKLM:\Software\Microsoft\Office\Outlook\Addins\Gecko.Ephorte.Outlook.Main' -Name 'LoadBehavior' -Value 0 -PropertyType dword).LoadBehavior}
+            if ($response -eq 0) {
+                "Success"
+            } else {
+                $response
+                "Failed"
+            }
+        }
+    } else {
+        "Computer is offline"
+    }
+}
