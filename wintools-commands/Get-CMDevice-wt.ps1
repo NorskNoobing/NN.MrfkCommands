7df8932171f3393c57d6505925d1d97675467067
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2022-06-26 15:59:51 +0200

    Remove files

--- wintools-commands/Get-CMDevice-wt.ps1
+++
@@ -1,9 +0,0 @@
-function Get-CMDevice-wt {
-    Invoke-Command -Credential $(Get-AdmCreds) -ComputerName wintools04 -ScriptBlock {
-        import-module 'C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\ConfigurationManager.psd1'
-        set-location ps1:
-        #BUG: Import-Module: The specified module was not loaded because no valid module file was found in any module directory.
-
-        Get-CMDevice @args
-    }
-}
