8d9874d0ce0f9330a405ca9a771e521f24da5eaf
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2022-08-25 17:59:04 +0200

    Remove deprecated

--- source/deprecated/one-time-use/MailToName.ps1
+++
@@ -1,8 +0,0 @@
-$testflow = Import-Excel Z:/'Test Flow.xlsx'
-$allmails = $testflow.Ansatt
-$namearray = New-Object -TypeName System.Collections.ArrayList
-foreach ($mail in $allmails) {
-    $displayname = Get-ADUser -Filter {mail -eq $mail}
-    $namearray.Add($displayname.givenname + " " + $displayname.surname)
-}
-Out-File -FilePath Z:/out.txt -InputObject $namearray
