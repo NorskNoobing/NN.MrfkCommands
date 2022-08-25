d068b6d0c2ca2fb2b58b8b54eb16dd45b941cf2b
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2022-08-25 10:00:50 +0200

    Update Invoke-WUServerBypass.ps1

--- source/Private/Invoke-WUServerBypass.ps1
+++ source/Private/Invoke-WUServerBypass.ps1
@@ -1,6 +1,9 @@
 function Invoke-WUServerBypass {
     #Enable WU
-    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Force
+    if (!(Test-Path -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU")) {
+        New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Force
+    }
+
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -name "DisableDualScan" -value 0
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -name "DisableWindowsUpdateAccess" -value 0
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -name "SetPolicyDrivenUpdateSourceForDriverUpdates" -value 0
