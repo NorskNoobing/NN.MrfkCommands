25191094f4548f255c3775357c6f11fbe014a18d
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2022-10-27 15:48:59 +0200

    Update New-LocalAdmin.ps1

--- source/Public/New-LocalAdmin.ps1
+++ source/Public/New-LocalAdmin.ps1
@@ -1,28 +1,56 @@
-# "-Enabled $true" and "-ChangePasswordAtLogon $true" doesn't seem to be working
 function New-LocalAdmin {
     param (
-        [Parameter(Mandatory=$true)][string]$username,
-        [Parameter(Mandatory=$true)][string]$hostname
+        [Parameter(Mandatory)][string]$username,
+        [Parameter(Mandatory)][string]$hostname
     )
     $admUsername = "adm_$username"
     $UPN = "$admUsername@intern.mrfylke.no"
 
     #Create user in AD
-    if (!(Get-ADUser -filter {UserPrincipalName -eq $UPN})) {
-        $result = Get-ADuser $username
+    if (!(Get-ADUser -Credential $(Get-AdmCreds) -filter {UserPrincipalName -eq $UPN})) {
+        $result = Get-ADuser -Credential $(Get-AdmCreds) $username
         $firstName = $result.GivenName
         $lastName = $result.Surname
         
-        New-ADUser -name $admUsername -Path "OU=Admin brukere,OU=\#Tilgangskontroll - Drift,OU=SADM,OU=MRFYLKE,DC=intern,DC=mrfylke,DC=no" -UserPrincipalName $UPN -Description "Lokaladmin på maskin $hostname" -GivenName "$firstName" -Surname "$lastName" -DisplayName "$firstName $lastName (Admin)" -AccountPassword (Read-Host -AsSecureString) -ChangePasswordAtLogon $true -Enabled $true
+        New-ADUser -Credential $(Get-AdmCreds) -name "$firstName $lastName (Admin)" -SamAccountName $admUsername -Path "OU=Lokaladmins,OU=Adminbrukere,DC=intern,DC=mrfylke,DC=no" -UserPrincipalName $UPN -Description "Lokaladmin på maskin $hostname" -GivenName "$firstName" -Surname "$lastName" -DisplayName "$firstName $lastName (Admin)" -AccountPassword ("Molde123!" | ConvertTo-SecureString -AsPlainText) -ChangePasswordAtLogon $true -Enabled $true
     } else {
-        "UPN $UPN already exists"
+        Write-Information -MessageData "UPN `"$UPN`" already exists" -InformationAction Continue
     }
 
+    $discordHookPath = "$env:USERPROFILE\.creds\Discord\workHookUri.xml"
+    if (!(Test-Path $discordHookPath)) {
+        $discordHookDir = $discordHookPath.Substring(0, $discordHookPath.lastIndexOf('\'))
+        if (!(Test-Path $discordHookDir)) {
+            New-Item -ItemType Directory $discordHookDir | Out-Null
+        }
+        Read-Host -AsSecureString "Enter Discord webhook uri" | Export-Clixml $discordHookPath
+    }
+
+    $taskName = "Localadmin for $admUsername on $hostname"
+    $task = Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue
+
     #Add ADUser to admin role on the given hostname
-    $Ping = (Test-Connection $hostname -Count 1).Status
-    if ($Ping -eq "Success") {
-        Invoke-Command -ComputerName $hostname -ScriptBlock {Add-LocalGroupMember -Group Administratorer -Member "intern\$admUsername"}
-    } else {
-        "Ping status: $Ping"
+    if (Test-Connection $hostname -Count 1 -Quiet) {
+        Invoke-Command -Credential $(Get-AdmCreds) -ComputerName $hostname -ScriptBlock {
+            if (Get-LocalGroupMember -Group Administratorer -Member "intern\$using:admUsername") {
+                "User `"$using:admUsername`" is already an administrator on `"$using:hostname`""
+            } else {
+                Add-LocalGroupMember -Group Administratorer -Member "intern\$using:admUsername"
+            }
+        }
+
+        if ($task) {
+            Send-DiscordMessage -uri (Import-Clixml $discordHookPath | ConvertFrom-SecureString -AsPlainText) -message "Task `"$taskName`" has been completed."
+            $task | Unregister-ScheduledTask -Confirm:$false
+        }
+    } elseif (!$task) {
+        $module1 = "C:\Users\danhol13\repos\mrfk-commands\Output\mrfk-commands.psm1"
+        $module2 = "C:\Users\danhol13\repos\NN.Notifications\Output\NN.Notifications.psm1"
+        $action = New-ScheduledTaskAction -Execute "pwsh.exe" -Argument "-WindowStyle Hidden -command `"Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force;Import-Module `"$module1`";Import-Module `"$module2`";New-LocalAdmin -username $username -hostname $hostname`""
+        #Run every 5 minutes
+        $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 5)
+        $null = Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Description "Add `"$admUsername`" as localadmin on `"$hostname`""
+
+        Send-DiscordMessage -uri (Import-Clixml $discordHookPath | ConvertFrom-SecureString -AsPlainText) -message "Task `"$taskName`" has been created."
     }
 }
