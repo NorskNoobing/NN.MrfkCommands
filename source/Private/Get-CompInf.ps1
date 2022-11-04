47a24a394f0b5e1c3a79b4b3d421f2cfd50b9c5f
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2022-11-04 15:41:19 +0100

    Update Get-CompInf.ps1
    * Removed logic to fetch unnessesary info

--- source/Private/Get-CompInf.ps1
+++ source/Private/Get-CompInf.ps1
@@ -15,49 +15,13 @@ function Get-CompInf {
         $adComputer = $allAdComputers.where({$_.Name -eq $hostname})
         $modelInfo = $allModelInfo.where({$_.ResourceID -eq $mecmComputer.ResourceId})
 
-        if ($mecmComputer.LastLogonUserName) {
-            try {
-                $adUser = Get-ADUser $mecmComputer.LastLogonUserName -Properties DisplayName
-            }
-            catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException] {
-                $null = $notes.Add("Can't find user with the identity `"$($mecmComputer.LastLogonUserName)`"")
-            }
-            catch [System.Management.Automation.ParameterBindingException] {
-                $null = $notes.Add("Can't convert parameter with name `"$($mecmComputer.LastLogonUserName)`" and type 'System.Object[]' to the type 'Microsoft.ActiveDirectory.Management.ADUser'")
-            }
-        } else {
-            $null = $notes.Add("There's no last logged on user")
-        }
-
         if ($mecmComputer.agentname) {
             $heartbeatIndex = $mecmComputer.agentname.IndexOf("Heartbeat Discovery")
             $lastHeartbeat = $mecmComputer.agenttime[$heartbeatIndex]
         }
-        
-        $IPv4 = $mecmComputer.IPAddresses.ForEach({
-            if ($_ -like "*.*") {
-                $_
-            }
-        })
-
-        #Get online status
-        try {
-            $tnc = Test-Connection $hostname -Count 1 -ErrorAction "Stop"
-
-            if ($tnc.Status -eq "TimedOut") {
-                [string]$status = "Offline"
-            }
-            elseif ($tnc.status -eq "Success") {
-                [string]$status = "Online"
-            }
-        }
-        catch [System.Net.NetworkInformation.PingException] {
-            [string]$status = "Can't resolve hostname"
-        }
 
         [PSCustomObject]@{
             Hostname          = $mecmComputer.Name
-            DisplayName       = $adUser.DisplayName
             Username          = $mecmComputer.LastLogonUserName
             MACAddress        = $mecmComputer.MACAddresses
             Model             = $modelInfo.Name
@@ -66,9 +30,7 @@ function Get-CompInf {
             CreatedAt         = $mecmComputer.CreationDate
             DeviceOSBuild     = $mecmComputer.BuildExt
             OS                = $mecmComputer.OperatingSystemNameandVersion
-            IPv4              = $IPv4
             "Enabled(AD)"     = $adComputer.Enabled
-            Status            = $status
             LastHeartbeat     = $lastHeartbeat
             CpuName           = $cpuInfo.Name
             Notes             = $notes
